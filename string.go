f620da3ba67c51c06a895024fe2a7e39da06caa8
akusirajakoding <akusirajakoding@gmail.com>
2026-01-31 16:32:56 +0700

    string

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Bruce")
+	fmt.Println("Bruce Wayne")
+	fmt.Println("Bruce Wayne Batman")
+
+	fmt.Println(len("Bruce"))
+	fmt.Println("Bruce Wayne"[0])
+	fmt.Println("Bruce Wayne Batman"[1])
+}
