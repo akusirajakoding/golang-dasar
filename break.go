c7cb18eb17a05486890f4f2d0684ba28b24a063a
akusirajakoding <akusirajakoding@gmail.com>
2026-02-04 20:48:55 +0700

    break

--- break.go
+++ break.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	for i := 0; i < 10; i++ {
 		if i == 5 {
-			fmt.Println("berhenti di perulangan ke-", i)
+			// fmt.Println("berhenti di perulangan ke-", i)
 			break
 	}
 		fmt.Println("perulangan ke-", i)
