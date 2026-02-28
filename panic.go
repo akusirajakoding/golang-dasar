1c60439a728be716672ef64b17fe9b1454dc755f
akusirajakoding <akusirajakoding@gmail.com>
2026-03-01 06:26:42 +0700

    panic

--- panic.go
+++ panic.go
@@ -1,1 +1,25 @@
 package main
+
+import "fmt"
+
+func endApp() {
+	fmt.Println("endApp")
+	message := recover()
+	fmt.Println("ups terjadi panic,", message)
+}
+
+func runApp(error bool) {
+	defer endApp()
+	if error {
+		panic("panic error")
+	}
+	// jika meletakkan recover di bawah panic yg error maka tidak akan terjadi recover
+	// karena jika terjadi panic kode di bawahnya tidak akan dilanjutkan kecuali defer
+	// oleh karena itu letakkan recover di dalam defer
+	//message := recover()
+	//fmt.Println("ups terjadi panic,", message)
+}
+
+func main() {
+	runApp(true)
+}
