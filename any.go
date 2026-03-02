bff65ed92efd908aba57be835e147aff81307889
akusirajakoding <akusirajakoding@gmail.com>
2026-03-03 04:45:28 +0700

    any

---
+++ any.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func getHello() any {
+	// return "Hello World"
+	// return 123
+	// return true
+	// return []string{"Hello", "World"}
+}
+
+func main() {
+	fmt.Println(getHello())
+}
