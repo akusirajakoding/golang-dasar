8d8cda6767ff0333af1bd703649a7a6f0b2dc148
akusirajakoding <akusirajakoding@gmail.com>
2026-03-06 21:14:08 +0700

    access modifier

--- import.go
+++ import.go
@@ -9,4 +9,8 @@ func main() {
 	result := helper.SayHello("Dono")
 	fmt.Println(result)
 
+	fmt.Println(helper.Application) // bisa mengakses variable Application yang ada di package helper, karena diawali dengan huruf besar
+	fmt.Println(helper.version) // tidak bisa mengakses variable version yang ada di package helper, karena diawali dengan huruf kecil
+	fmt.Println(helper.sayGoodbye("Dono")) // tidak bisa mengakses function sayGoodbye yang ada di package helper, karena diawali dengan huruf kecil
+
 }
